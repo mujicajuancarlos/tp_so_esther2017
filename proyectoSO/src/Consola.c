6216cb55a5b3c8a892310555cf23c09417a40966
FaridUtn <faridfafu@hotmail.com>
2017-04-04 15:43:34 -0300

    Update Consola.c

--- proyectoSO/src/Consola.c
+++ proyectoSO/src/Consola.c
@@ -13,55 +13,53 @@
 #include <unistd.h>
 #include <netdb.h>
 
-#define PORT 3550
+
+#define PUERTO 5000
 #define MAXIMODATOS 30
 
-
-int main(int argc , char *argv[]){
-	//aca falta agregar los argumenos para comprobar errores desde linea de comando
+int main(int argc, char *argv[]){
 
 int newSocket, bytesRecibidos;//ficheros
-char buffer[MAXIMODATOS]; //ACA SE GUARDA LOQ  SE RECIBE
 
-struct hostent *he;
+char buffer[MAXIMODATOS];//aca se guarda lo q se recibe
+
+struct hostent *he //guarda datos de la maquina
+
 struct sockaddr_in servidor;
 
-//ACA COMPROBACION DE ERRORES DE LINEA DE COMANDO
-if(argc !=2){
-	printf("Se usa: %s",argv[0]);
-			exit(-1);
+if(argc !=2){ //porq el parametro es la ip solamente
+printf("Error"); //aca es donde imprimia un directorio en vez de la ip
+exit(-1);
 }
 
-if((he=gethostbyname(argv[1]))==NULL){
-	printf("gethostbyname error");
-	exit(-1);
+if((he= gethostbyname(argv[1])) == NULL){
+printf("error en geth");
+exit(-1);
 }
 
 if((newSocket=socket(AF_INET,SOCK_STREAM,0)) == -1){
-	printf("Error en socket");
+
+printf("Error en socket");
 exit(-1);
 }
 
 servidor.sin_family = AF_INET;
-servidor.sin_port = 0;
+servidor.sin_port = 0;//puede ir htons(PUERTO)
 servidor.sin_addr = *((struct in_addr *)he->h_addr);
 bzero(&(servidor.sin_zero),8);
-//inet_ntoa(*((struct in_addr *)he->h_addr))
-if(connect(newSocket,(struct sockaddr *)&servidor, sizeof(struct sockaddr)) == -1){
-	printf("Error en connect");
-	exit(-1);
 
+if(connect(newSocket,(struct sockaddr *)&servidor, sizeof(struct sockaddr)) == -1{
+printf("Error en conexion");
+exit(-1);
 }
 
-if((bytesRecibidos=recv(newSocket,buffer,MAXIMODATOS,0)) == -1){
-	printf("Error en recv");
-	exit(-1);
-
+if((bytesRecibidos = recv(newSocket,buffer,MAXIMODATOS,0)) == -1) {
+printf("Error en recv");
+exit(-1);
 }
 
-buffer[bytesRecibidos] = //PONER BARRA CERO Y TERMINAR CADENA
-
-printf("Mensaje recibido desde el servidor: %s", buffer);
-
+buffer[bytesRecibidos] = '\0'
+printf("Mensaje recibido desde el servidor: %s",buffer);
 close(newSocket);
 }
+   
