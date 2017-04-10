dfec063701f402bbf839de8d22dbbc771ffd9029
chrismedina13 <chrismedina13@hotmail.com>
2017-04-10 00:22:36 -0300

    pequeños arreglos en consola

--- console/src/console.c
+++ console/src/console.c
@@ -10,8 +10,12 @@
 #include "console.h"
 #include "configuration/configuration.h"
 #include "socket.h"
+#define MAXDATASIZE 100
 
 int main(int argc, char *argv[]) {
+
+	int numbytes;
+	char buf[MAXDATASIZE];
 
 	puts("Accediendo al archivo de configuración...");
 	Configuration* config = config_with(argc > 1 ? argv[1] : NULL);
@@ -23,6 +27,12 @@ int main(int argc, char *argv[]) {
 		t_log* logger = log_create(config->log_file, config->log_program_name, config->log_print_console, log_level_from_string(config->log_level));
 
 	log_info(logger, "Inició el proceso correctamente!");
+
+	int sckCliente = crearSocketCliente(config->ip_kernel,config->puerto_kernel);
+
+	if((send(sckCliente,"Hello, world!\n",14,0)) == -1){
+		puts("error en el envio");
+	}
 
 	return EXIT_SUCCESS;
 }
