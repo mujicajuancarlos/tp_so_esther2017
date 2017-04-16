8b3e6fab5a1b7464a6f8440c98b4ab5cc4192a5a
Agustin Meinardo <agnmeinardo@outlook.com>
2017-04-16 17:33:55 -0300

    Agregando CODs de Protocolos Memory-Kernel y Memory-CPU

--- dc-commons/src/dc-commons/protocol-memory-cpu.h
+++ dc-commons/src/dc-commons/protocol-memory-cpu.h
@@ -8,6 +8,10 @@
 #ifndef SRC_DC_COMMONS_PROTOCOL_MEMORY_CPU_H_
 #define SRC_DC_COMMONS_PROTOCOL_MEMORY_CPU_H_
 
+#define COD_SOLICITAR_BYTES_PAGE 6 // Solicitar bytes de una página
+#define COD_ESCRITURA_PAGE 7 // Almacenar bytes en una página
+#define COD_INFO_PROGRAMA_EXE 8 //?
+#define COD_ACTUALIZAR_ESTRUCTURA 9 //?
 #define COD_SALUDO 100
 
 #endif /* SRC_DC_COMMONS_PROTOCOL_MEMORY_CPU_H_ */
