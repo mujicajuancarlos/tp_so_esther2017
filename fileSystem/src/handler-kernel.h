6dd340da71bb70c9ff8d104a2ba2a245b270c0aa
Joel Akiyoshi <joelakiy.23@gmail.com>
2017-04-21 18:43:18 -0300

    Envío de packages 2/3

--- fileSystem/src/handler-kernel.h
+++ fileSystem/src/handler-kernel.h
@@ -11,14 +11,18 @@
 #include <stddef.h>
 #include <errno.h>
 
+#include "fileSystem.h"
+
 #include <dc-commons/package.h>
 #include <dc-commons/socket-message.h>
 #include <dc-commons/protocol-fileSystem-kernel.h>
-#include "fileSystem.h"
 
 void handleKernel(fileSystem_struct *args);
 
 void handleKernelRequest(fileSystem_struct *args, Package *package);
+
+void exceptionTo(int fileDescriptor, char *exception);
+
 
 
 #endif /* HANDLER_KERNEL_H_ */
