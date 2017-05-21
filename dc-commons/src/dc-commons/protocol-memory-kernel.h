c45b5974b4536a9efa36d88913798953c633efa4
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-05-20 23:44:32 -0300

    Agrego codigos para comunicacion entre kernel y memoria

--- dc-commons/src/dc-commons/protocol-memory-kernel.h
+++ dc-commons/src/dc-commons/protocol-memory-kernel.h
@@ -16,17 +16,60 @@
 
 #define COD_HANDSHAKE_KERNEL 400
 
-#define COD_SAVE_NEW_PROGRAM 401 //para almacenar el cod ansisop
-#define COD_SAVE_NEW_PROGRAM_OK 402 //respuesta ok para el cod COD_SAVE_NEW_PROGRAM
-#define COD_PAGE_SIZE_REQUEST 403 //para solicitar el tamaño de pagina
-#define COD_PAGE_SIZE_RESPONSE 404 //para devolver el tmaño de pagina
+/*
+ * Solicitar el tamaño de pagina
+ *	Parámetros: n/a
+ */
+#define COD_PAGE_SIZE_REQUEST 401 //para solicitar el tamaño de pagina
+#define COD_PAGE_SIZE_RESPONSE 402 //para devolver el tmaño de pagina
 
-#define COD_MEMORY_FULL 409 //indica que no hay mas espacio
+
+//definicion de las operaciones de la memoria (pag 26)
+
+/*
+ * Inicializar programa
+ *	Parámetros: [Identificador del Programa] [Páginas requeridas]
+ */
+#define COD_NEW_PROCESS_REQUEST 403
+#define COD_NEW_PROCESS_RESPONSE 404
+
+/*
+ * Solicitar bytes de una página
+ *	Parámetros: [Identificador del Programa] [Páginas requeridas]
+ */
+#define COD_GET_PAGE_BYTES_REQUEST 405
+#define COD_GET_PAGE_BYTES_RESPONSE 406
+
+/*
+ * Almacenar bytes en una página
+ *	Parámetros: [Identificador del Programa], [#página], [offset], [tamaño] y [buffer]
+ */
+#define COD_SAVE_PAGE_BYTES_REQUEST 408
+#define COD_SAVE_PAGE_BYTES_RESPONSE 409
+
+/*
+ * Asignar Páginas a Proceso
+ *	Parámetros: [Identificador del Programa] [Páginas requeridas]
+ */
+#define COD_ADD_PROCESS_PAGES_REQUEST 410
+#define COD_ADD_PROCESS_PAGES_RESPONSE 411
+
+/*
+ * Finalizar programa
+ *	Parámetros: [Identificador del Programa]
+ */
+#define COD_END_PROCESS_REQUEST 410
+#define COD_END_PROCESS_RESPONSE 411
+
+
+/*
+ * codigo de error para indicar que no hay mas memoria todo mover a un archivo global de errores
+ */
+#define COD_MEMORY_FULL 444 //indica que no hay mas espacio
 
 /**
  * todo seguir definiendo
  */
-
 
 /**
  * Estructuras y funciones recervar paginas para un nuevo proceso
@@ -37,7 +80,8 @@ typedef struct {
 	uint32_t sourceCodeSize;
 	char* sourceCode;
 } t_new_sourceCode_request;
-t_new_sourceCode_request* create_new_sourceCode_request(uint32_t pid, uint32_t stackSize, uint32_t sourceCodeSize, char* sourceCode);
+t_new_sourceCode_request* create_new_sourceCode_request(uint32_t pid,
+		uint32_t stackSize, uint32_t sourceCodeSize, char* sourceCode);
 void destroy_new_sourceCode_request(t_new_sourceCode_request* request);
 size_t size_new_sourceCode_request(t_new_sourceCode_request* request);
 char* serialize_new_sourceCode_request(t_new_sourceCode_request* request);
