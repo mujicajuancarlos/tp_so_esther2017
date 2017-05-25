ab20dfd05755b481a8ac3ad18ba0b4e097f4450a
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-05-25 01:24:29 -0300

    Fixes de file descriptor

--- kernel/src/module.model/fileDescriptor.c
+++ kernel/src/module.model/fileDescriptor.c
@@ -7,112 +7,110 @@
 #include "fileDescriptor.h"
 
 t_list* tablaGlobalFD;
-static int fd=3;
-static int aux;
+int nextFD = FIRST_FD;
 
-pthread_mutex_t fileDescriptorMutex;
-pthread_mutex_t tablaGlobalFDMutex;
+pthread_mutex_t fileDescriptor_mutex;
+pthread_mutex_t tablaGlobalFD_mutex;
+pthread_mutex_t nextFD_mutex;
 
-void initializetablaGlobalFDMutex() {
-	pthread_mutex_init(&tablaGlobalFDMutex, NULL);
+void initializeFileSystemModule() {
+	logInfo("Inicializando el modulo FS");
+	pthread_mutex_init(&tablaGlobalFD_mutex, NULL);
+	pthread_mutex_init(&fileDescriptor_mutex, NULL);
+	pthread_mutex_init(&nextFD_mutex, NULL);
+	tablaGlobalFD = list_create();
 }
 
-void destroytablaGlobalFDMutex() {
-	pthread_mutex_destroy(&tablaGlobalFDMutex);
+void destroyFileSystemModule() {
+	logInfo("Destruyendo el modulo FS");
+	pthread_mutex_destroy(&tablaGlobalFD_mutex);
+	pthread_mutex_destroy(&fileDescriptor_mutex);
+	pthread_mutex_destroy(&nextFD_mutex);
+	list_destroy_and_destroy_elements(tablaGlobalFD,
+			(void*) destroy_t_filedescriptor);
 }
 
-void tablaGlobalFDMutex_lock() {
-	pthread_mutex_lock(&tablaGlobalFDMutex);
-
+void tablaGlobalFD_mutex_lock() {
+	pthread_mutex_lock(&tablaGlobalFD_mutex);
 }
 
-void tablaGlobalFDMutex_unlock() {
-	pthread_mutex_unlock(&tablaGlobalFDMutex);
-
+void tablaGlobalFD_mutex_unlock() {
+	pthread_mutex_unlock(&tablaGlobalFD_mutex);
 }
 
-void initializefileDescriptors() {
-	pthread_mutex_init(&fileDescriptorMutex, NULL);
+void fileDescriptor_mutex_lock() {
+	pthread_mutex_lock(&fileDescriptor_mutex);
 }
 
-void destroyfileDescriptors() {
-	pthread_mutex_destroy(&fileDescriptorMutex);
+void fileDescriptor_mutex_unlock() {
+	pthread_mutex_unlock(&fileDescriptor_mutex);
 }
 
-void fileDescriptors_lock() {
-	pthread_mutex_lock(&fileDescriptorMutex);
-
+void nextFD_mutex_lock() {
+	pthread_mutex_lock(&nextFD_mutex);
 }
 
-void fileDescriptors_unlock() {
-	pthread_mutex_unlock(&fileDescriptorMutex);
-
+void nextFD_mutex_unlock() {
+	pthread_mutex_unlock(&nextFD_mutex);
 }
 
-t_fileDescriptor* createNew_t_fileDescriptor(char* path){
+t_fileDescriptor* createNew_t_fileDescriptor(char* path) {
 
 	t_fileDescriptor* newFD = malloc(sizeof(t_fileDescriptor));
-	newFD->fd = getFD();
-	newFD->path = path;
-	newFD->open= 0;
+	newFD->fd = getNextFD();
+	newFD->open = 0;
+	size_t sizeBuffer = strlen(path); //asegurarse que el que lo llame pase un string y no un stream
+	newFD->path = calloc(1, sizeBuffer);
+	memcpy(newFD->path, path, sizeBuffer);
 
 	return newFD;
 }
 
-void destroy_t_filedescriptor(t_fileDescriptor* fd){
-	if(fd->path != NULL){
-
+void destroy_t_filedescriptor(t_fileDescriptor* fd) {
+	if (fd->path != NULL) {
 		free(fd->path);
 	}
 	free(fd);
+}
 
-};
-
-int getFD(){
-	fileDescriptors_lock();
-	aux=fd;
-	fd++;
-	fileDescriptors_unlock();
+int getNextFD() {
+	int aux;
+	nextFD_mutex_lock();
+	aux = nextFD;
+	nextFD++;
+	nextFD_mutex_unlock();
 	return aux;
 }
 
-
-
-t_list* inicializarTablaGlobal(){
-	tablaGlobalFD = list_create();
-	return tablaGlobalFD;
+void agregarFD_Alista(t_fileDescriptor* fd) {
+	tablaGlobalFD_mutex_lock();
+	list_add(tablaGlobalFD, fd);
+	tablaGlobalFD_mutex_unlock();
 }
 
-
-void agregarFD_Alista(t_fileDescriptor* fd,t_list* lista){
-	tablaGlobalFDMutex_lock();
-	list_add(lista, fd);
-	tablaGlobalFDMutex_unlock();
-}
-
-
-void removerFD_Lista(t_fileDescriptor* fd, t_list* lista){
-	bool condicion(void* element){
+void removerFD_Lista(t_fileDescriptor* fd) {
+	bool condicion(void* element) {
 		t_fileDescriptor* unFD = element;
-		return unFD->path == fd->path;
+		return unFD == fd;
 	}
-	tablaGlobalFDMutex_lock();
-	list_remove_and_destroy_by_condition(lista,condicion,destroy_t_filedescriptor);
-	tablaGlobalFDMutex_unlock();
+	tablaGlobalFD_mutex_lock();
+	list_remove_and_destroy_by_condition(tablaGlobalFD, condicion,
+			(void*) destroy_t_filedescriptor);
+	tablaGlobalFD_mutex_unlock();
 }
 
-void incrementarOpen(t_fileDescriptor* fd){
-	tablaGlobalFDMutex_lock();
-	fd->open = fd->open +1;
-	tablaGlobalFDMutex_unlock();
+void incrementarOpen(t_fileDescriptor* fd) {
+	fileDescriptor_mutex_lock();
+	fd->open = fd->open + 1;
+	fileDescriptor_mutex_unlock();
 }
 
-void decrementarOpen(t_fileDescriptor* fd){
-	tablaGlobalFDMutex_lock();
-	if(fd->open <=1){
-		removerFD_Lista(fd,tablaGlobalFD);
+void decrementarOpen(t_fileDescriptor* fd) {
+	fileDescriptor_mutex_lock();
+	fd->open--;
+	if (fd->open == 0) {
+		removerFD_Lista(fd);
 	}
-	fd->open = fd->open -1;
-	tablaGlobalFDMutex_unlock();
+	fileDescriptor_mutex_unlock();
 }
 
--- kernel/src/module.model/fileDescriptor.h
+++ kernel/src/module.model/fileDescriptor.h
@@ -10,7 +10,13 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <pthread.h>
+#include <dc-commons/logger.h>
 #include <commons/collections/list.h>
+
+#define FIRST_FD 3
 
 typedef struct {
 	uint32_t fd; //numero de FileDescriptor
@@ -18,16 +24,27 @@ typedef struct {
 	uint32_t open; //cantidad de procesos que lo tienen abierto
 }t_fileDescriptor;
 
+void initializeFileSystemModule();
+void destroyFileSystemModule();
 
-void initializefileDescriptors();
+void tablaGlobalFD_mutex_lock();
+void tablaGlobalFD_mutex_unlock();
 
-void destroyfileDescriptors();
+void fileDescriptor_mutex_lock();
+void fileDescriptor_mutex_unlock();
 
-void fileDescriptors_lock();
+void nextFD_mutex_lock();
+void nextFD_mutex_unlock();
 
-void fileDescriptors_unlock();
+t_fileDescriptor* createNew_t_fileDescriptor(char* path);
+void destroy_t_filedescriptor(t_fileDescriptor* fd);
 
-t_fileDescriptor* createNew_t_fileDescriptor(char*);
-void destroy_t_filedescriptor(t_fileDescriptor*);
-int getFD();
+int getNextFD();
+
+void agregarFD_Alista(t_fileDescriptor* fd);
+void removerFD_Lista(t_fileDescriptor* fd);
+
+void incrementarOpen(t_fileDescriptor* fd);
+void decrementarOpen(t_fileDescriptor* fd);
+
 #endif /* MODULE_MODEL_FILEDESCRIPTOR_H_ */
