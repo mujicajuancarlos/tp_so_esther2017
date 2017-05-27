e0cca1a0e979e29585bf085b1d53b7d508b25e88
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-05-27 04:12:51 -0300

    Agrego protocolo de comunicacion entre cpu y memoria

--- dc-commons/src/dc-commons/protocol-memory-cpu.h
+++ dc-commons/src/dc-commons/protocol-memory-cpu.h
@@ -12,10 +12,26 @@
 
 #define COD_HANDSHAKE_CPU 300
 
-#define COD_SOLICITAR_BYTES_PAGE 301 // Solicitar bytes de una página
-#define COD_ESCRITURA_PAGE 302 // Almacenar bytes en una página
-#define COD_INFO_PROGRAMA_EXE 303 //?
-#define COD_ACTUALIZAR_ESTRUCTURA 304 //?
-#define COD_PROXIMA_SENTENCIA_A_EJECUTAR 305
+/*
+ * Solicitar el tamaño de pagina
+ *	Parámetros: n/a
+ */
+#define COD_PAGE_SIZE_REQUEST 301 //para solicitar el tamaño de pagina
+#define COD_PAGE_SIZE_RESPONSE 302 //para devolver el tmaño de pagina
+
+/*
+ * Solicitar bytes de una página
+ *	Parámetros: [Identificador del Programa], [#página], [offset] y [tamaño]
+ */
+#define COD_GET_PAGE_BYTES_REQUEST 303
+#define COD_GET_PAGE_BYTES_RESPONSE 304
+
+/*
+ * Almacenar bytes en una página
+ *	Parámetros: [Identificador del Programa], [#página], [offset], [tamaño] y [buffer]
+ */
+#define COD_SAVE_PAGE_BYTES_REQUEST 305
+#define COD_SAVE_PAGE_BYTES_RESPONSE 304
+
 
 #endif /* SRC_DC_COMMONS_PROTOCOL_MEMORY_CPU_H_ */
