15218a008f6ae81a5dd471e70cafb6f7c6b98f26
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-05-27 01:01:14 -0300

    Agrego codigos de error genericos de la aplicacion

---
+++ dc-commons/src/dc-commons/protocol-errors.h
@@ -0,0 +1,15 @@
+/*
+ * protocol-errors.h
+ *
+ *  Created on: 26/5/2017
+ *      Author: utnso
+ */
+
+#ifndef SRC_DC_COMMONS_PROTOCOL_ERRORS_H_
+#define SRC_DC_COMMONS_PROTOCOL_ERRORS_H_
+
+
+#define ERROR_DISCONNECTED_SOCKET -10
+#define ERROR_UNKNOWN_MESSAGE_CODE -11
+
+#endif /* SRC_DC_COMMONS_PROTOCOL_ERRORS_H_ */
