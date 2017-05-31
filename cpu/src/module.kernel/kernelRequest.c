cebaa10fe3355db033918eea5f62b261887ad28d
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-05-31 02:52:00 -0300

    fix kernelRequests

--- cpu/src/module.kernel/kernelRequest.c
+++ cpu/src/module.kernel/kernelRequest.c
@@ -7,27 +7,31 @@
 
 #include "kernelRequest.h"
 
-void updateStackPageSize(cpu_struct* cpuStruct, Package* package){
+void updateStackPageSize(cpu_struct* cpuStruct, Package* package) {
 	int size = deserialize_int(package->stream);
-	logInfo("El kernel indico que la cantidad de paginas permitidas para el stack es %d", size);
+	logInfo(
+			"El kernel indico que la cantidad de paginas permitidas para el stack es %d",
+			size);
 	setStackPageSize(size);
 }
 
-void executeNewPcb(cpu_struct* cpuStruct, Package* package){
+void executeNewPcb(cpu_struct* cpuStruct, Package* package) {
 	PCB* pcb = deserialize_PCB(package->stream);
+	if (pcb->stackIndex == NULL)
+		createNewContext(pcb);
 	loadPCB(pcb);
 	logTrace("El PCB para pid: %d fue cargado", pcb->pid);
 	ansisopExecuteInstruccion(cpuStruct);
 	logTrace("Se ejecuto la instruccion ansisop del pid: %d", pcb->pid);
 }
 
-void continueExecution(cpu_struct* cpuStruct, Package* package){
+void continueExecution(cpu_struct* cpuStruct, Package* package) {
 	logTrace("Continuando la ejecucion de la siguiente instruccion ansisop");
 	ansisopExecuteInstruccion(cpuStruct);
-	logTrace("Se ejecuto la instruccion ansisop del pid: %d", pcb->pid);
+	logTrace("Se ejecuto la instruccion ansisop del pid: %d", getPCB()->pid);
 }
 
-void contextSwitch(cpu_struct* cpuStruct, Package* package){
+void contextSwitch(cpu_struct* cpuStruct, Package* package) {
 	logTrace("Descargando el actual pcb");
 	unloadPCB(cpuStruct);
 }
