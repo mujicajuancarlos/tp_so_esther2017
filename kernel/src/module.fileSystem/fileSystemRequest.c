7e21be0c231675c6ffba719ec4060d225d74cce9
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-06-01 02:54:20 -0300

    Agrego directorio y archivos para definir las funciones que solicitan datos al file system

---
+++ kernel/src/module.fileSystem/fileSystemRequest.c
@@ -0,0 +1,14 @@
+/*
+ * fileSystemRequest.c
+ *
+ *  Created on: 1/6/2017
+ *      Author: utnso
+ */
+
+#include "fileSystemRequest.h"
+
+void validarArchivo(kernel_struct* kernelStruct){
+
+
+
+}
---
+++ kernel/src/module.fileSystem/fileSystemRequest.h
@@ -0,0 +1,18 @@
+/*
+ * fileSystemRequest.h
+ *
+ *  Created on: 1/6/2017
+ *      Author: utnso
+ */
+
+#ifndef MODULE_FILESYSTEM_FILESYSTEMREQUEST_H_
+#define MODULE_FILESYSTEM_FILESYSTEMREQUEST_H_
+
+#include <dc-commons/protocol-fileSystem-kernel.h>
+#include "../kernelStruct.h"
+#include <dc-commons/package.h>
+#include <dc-commons/socket-message.h>
+
+void validarArchivo(kernel_struct* kernelStruct);
+
+#endif /* MODULE_FILESYSTEM_FILESYSTEMREQUEST_H_ */
