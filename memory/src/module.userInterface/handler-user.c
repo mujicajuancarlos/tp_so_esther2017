f97c8b74ea054afff1b0bdee520a43c3668f3020
NicoUTN <nicolaswallovits@hotmail.com>
2017-06-03 02:47:16 -0300

    Armado de estructura para manejar consola de la memoria

--- memory/src/module.userInterface/handler-user.c
+++ memory/src/module.userInterface/handler-user.c
@@ -45,6 +45,14 @@ void handleUserRequest(memory_struct* memoryStruct){
 				shouldCompareCommand = false;
 				handleCommand_set_sleep (memoryStruct, commands);
 			}
+			if (shouldCompareCommand && equal_user_command(commands[0], COD_CONSOLE_FLUSH)) {
+				shouldCompareCommand = false;
+				handleCommand_flush (memoryStruct, commands);
+			}
+			if (shouldCompareCommand && equal_user_command(commands[0], COD_CONSOLE_SIZE)) {
+				shouldCompareCommand = false;
+				handleCommand_size (memoryStruct, commands);
+			}
 		}
 
 		if (shouldCompareCommand) {
@@ -91,9 +99,14 @@ void handleCommand_dump_structure(memory_struct* memoryStruct,
 		printInvalidArguments(commands[2], commands[0]);
 }
 
-void handleCommand_dump_content(memory_struct* kernelStruct, char** commands) {
-	if (commands[2] == NULL) {
-		printf ("\ datos almacenados en memoria de todos los proc o de alguno en particular\n");
+void handleCommand_dump_content(memory_struct* memoryStruct, char** commands) {
+	if (commands[1] != NULL) {
+		if (equal_user_command(commands[2], OPT_ALL)) {
+			handleCommand_dump_content_all (memoryStruct, commands);
+		}
+		else if (commands[2] != NULL) {
+			handleCommand_dump_content_pid(memoryStruct, commands);
+		}
 /*
  * TODO datos almacenados en la memoria de todos los procesos o de alguno en particular
  */
@@ -101,38 +114,72 @@ void handleCommand_dump_content(memory_struct* kernelStruct, char** commands) {
 		printInvalidArguments(commands[2], commands[0]);
 }
 
+void handleCommand_dump_content_all (memory_struct* memoryStruct, char** commands) {
+	if (commands[3] == NULL) {
+		printf ("\n dump de contenido completo \n");
+	} else
+		printInvalidArguments(commands[2], commands[0]);
+}
+
+void handleCommand_dump_content_pid (memory_struct* memoryStruct, char** commands) {
+	if (commands[3] == NULL) {
+		int pid = atoi (commands[2]);
+		printf ("\n dump de contenido completo de proceso %i \n", pid);
+	} else
+		printInvalidArguments(commands[2], commands[0]);
+}
+
 void handleCommand_set_sleep (memory_struct* memoryStruct, char** commands) {
 
-	if (commands[1] != NULL) {
-		if (equal_user_command(commands[1], OPT_MILISECONDS)) {
-			printf ("\n el sleep se ha modificado \n");
-			handleCommand_set_sleep_to_value(memoryStruct, commands);
-		}
+	if (commands[1] != NULL && commands[2] == NULL) {
+		int sleep = atoi (commands[1]);
+		printf ("\n sleep fue modificado a %i \n", sleep);
 	} else
 		printInvalidArguments("", commands[0]);
 
 }
 
-void handleCommand_set_sleep_to_value (memory_struct* memoryStruct, char** commands) {
-	int maxValue = 100;
-	int minValue = 0;
+void handleCommand_flush (memory_struct* memoryStruct, char** commands) {
+	if (commands[1] != NULL) {
+		if (equal_user_command(commands[1], OPT_CACHE)) {
+			handleCommand_flush_cache(memoryStruct, commands);
+		}
+	} else
+			printInvalidArguments("", commands[0]);
+}
 
+void handleCommand_flush_cache (memory_struct* memoryStruct, char** commands) {
+	if (commands[2] == NULL) {
+		printf ("\n se hace flush de cache\n");
+	/*
+	 * TODO flush completo de la cache
+	 */
+	} else
+		printInvalidArguments(commands[2], commands[0]);
+}
 
+void handleCommand_size (memory_struct* memoryStruct, char** commands) {
+	if (commands[1] != NULL) {
+		if (equal_user_command(commands[1], OPT_MEMORY)) {
+			handleCommand_size_memory (memoryStruct, commands);
+		}
+		if (equal_user_command(commands[1], OPT_PID)) {
+			handleCommand_size_pid (memoryStruct, commands);
+		}
+	} else
+			printInvalidArguments("", commands[0]);
+}
+
+void handleCommand_size_memory (memory_struct* memoryStruct, char** commands) {
+	printf ("\nEl tamaño de la memoria es de X \n");
+}
+
+void handleCommand_size_pid (memory_struct* memoryStruct, char** commands) {
 	if (commands[2] != NULL && commands[3] == NULL) {
-		int newValue = atoi (commands[2]);
-
-		if (newValue > maxValue)
-			memoryStruct->memorySleep = maxValue;
-		else if (newValue < minValue)
-			memoryStruct->memorySleep = minValue;
-		else
-			memoryStruct->memorySleep = newValue;
-	}
-
-	else
+		int pid = atoi (commands[2]);
+		printf ("\n El tamaño del proceso %i es de X\n", pid);
+	} else
 		printInvalidArguments("", commands[0]);
-
-	printf ("%i\n", memoryStruct->memorySleep);
 }
 
 void printCommandsHelp() {
@@ -143,16 +190,29 @@ void printCommandsHelp() {
 	printf("\nForma de uso: «command» [«option»] \n\n");
 	puts("Lista de comandos permitidos:");
 
-	printf(patter,
-	COD_CONSOLE_DUMP, OPT_ALL,
-			"Muestra informacion de todos las paginas");
-	printf(patter,
-			COD_CONSOLE_DUMP, OPT_PAGE,
-			"Muestra informacion de una página especifica «page»");
+	printf(patter, COD_CONSOLE_DUMP, OPT_CACHE,
+			"Dump completo de memoria cache");
 
-	printf(patter, COD_CONSOLE_CLEAR, "", "", "Limpia la pantalla");
+	printf(patter, COD_CONSOLE_DUMP, OPT_STRUCTURE,
+			"Muestra tabla de páginas y listado de procesos activos");
 
-	printf(patter, COD_CONSOLE_EXIT, "", "", "Finaliza el proceso");
+	printf(patter, COD_CONSOLE_DUMP, OPT_CONTENT,
+			"Muestra datos almacenados en memoria de todos los procesos «-all» o de un proceso en particular «pid»");
+
+	printf(patter, COD_CONSOLE_FLUSH, OPT_CACHE,
+			"Limpia completamente el contenido de la cache");
+
+	printf(patter, COD_CONSOLE_SIZE, OPT_MEMORY,
+			"Tamanio de la memoria en frames. Cantidad de frames ocupados y libres");
+
+	printf(patter, COD_CONSOLE_SIZE, OPT_DESIRED_PID,
+			"Tamaño total de un proceso");
+
+	printf(patter, COD_CONSOLE_CLEAR, "", "",
+			"Limpia la pantalla");
+
+	printf(patter, COD_CONSOLE_EXIT, "", "",
+			"Finaliza el proceso");
 
 	unlockPrinter();
 }
