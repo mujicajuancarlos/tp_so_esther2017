2c722027c70ba329bac2d854beb8276a234f7cde
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-06-10 17:29:26 -0300

    Implemento la funcion que recibe el mensaje de error de ejecucion desde la cpu

--- kernel/src/module.cpu/cpuRequests.c
+++ kernel/src/module.cpu/cpuRequests.c
@@ -184,11 +184,11 @@ void executeSetSharedVar(CPU* cpu, Package* package) {
 	if (setSharedVar(data->name, data->newValue) == UPDATE_VAR_SUCCESS) {
 		char* buffer = serialize_int(data->newValue);
 		tmpPackage = createAndSendPackage(cpu->fileDescriptor,
-						COD_SYSCALL_SUCCESS, sizeof(int), buffer);
+		COD_SYSCALL_SUCCESS, sizeof(int), buffer);
 		free(buffer);
 	} else {
 		tmpPackage = createAndSendPackage(cpu->fileDescriptor,
-				COD_SYSCALL_FAILURE, 0, NULL);
+		COD_SYSCALL_FAILURE, 0, NULL);
 		moveFromExcecToExit_withError(cpu->process, SC_ERROR_SET_SHARED_VAR);
 		markFreeCPU(cpu);
 	}
@@ -203,11 +203,11 @@ void executeGetSharedVar(CPU* cpu, Package* package) {
 	if (getSharedVar(key, &value) == UPDATE_VAR_SUCCESS) {
 		char* buffer = serialize_int(value);
 		tmpPackage = createAndSendPackage(cpu->fileDescriptor,
-				COD_SYSCALL_SUCCESS, sizeof(int), buffer);
+		COD_SYSCALL_SUCCESS, sizeof(int), buffer);
 		free(buffer);
 	} else {
 		tmpPackage = createAndSendPackage(cpu->fileDescriptor,
-				COD_SYSCALL_FAILURE, 0, NULL);
+		COD_SYSCALL_FAILURE, 0, NULL);
 		moveFromExcecToExit_withError(cpu->process, SC_ERROR_GET_SHARED_VAR);
 		markFreeCPU(cpu);
 	}
@@ -280,7 +280,11 @@ void resolveRequest_updateSemaphore(CPU* cpu, Package* package) {
 	}
 }
 
+/*
+ * cuando la cpu lanza un error invoca al kernel esta funcion para que el proceso finalize y libere la cpu
+ */
 void resolveRequest_executionError(CPU* cpu, Package* package) {
-	//todo enviar el proceso a exit con el exit code
-	//la cpu queda limpia
+	uint32_t exitCode = deserialize_int(package->stream);
+	moveFromExcecToExit_withError(cpu->process, exitCode);
+	markFreeCPU(cpu);
 }
