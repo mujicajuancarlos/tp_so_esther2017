4ace21b9331596cb8b9b3dc324acaa431dd6b489
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-07-03 20:48:27 -0300

    fix - corrige log de stdout 1

--- kernel/src/module.console/consoleResponse.c
+++ kernel/src/module.console/consoleResponse.c
@@ -19,7 +19,7 @@ void consoleResponseRepulseMessage(Process* newProcess) {
 void runProgramIsOK_response(Process* process) {
 	char* buffer = serialize_int(process->pid);
 	Package* package = createAndSendPackage(process->fileDescriptor,
-			COD_KC_RUN_PROGRAM_RESPONSE, sizeof(uint32_t), buffer);
+	COD_KC_RUN_PROGRAM_RESPONSE, sizeof(uint32_t), buffer);
 	free(buffer);
 	destroyPackage(package);
 }
@@ -28,17 +28,23 @@ void notifyEndProcess(Process* process) {
 	Package* package;
 	char* exitCode = serialize_int(process->exit_code);
 	package = createAndSendPackage(process->fileDescriptor,
-		COD_KC_END_PROGRAM, sizeof(uint32_t), exitCode);
+	COD_KC_END_PROGRAM, sizeof(uint32_t), exitCode);
 	free(exitCode);
 	logInfo("Indico la finalizacion del proceso a la Consola %d", process->pid);
 	destroyPackage(package);
 }
 
-void writeOnStdout(Process* process, t_data_FD_request* dataRequest){
+void writeOnStdout(Process* process, t_data_FD_request* dataRequest) {
 	Package* package;
-	package = createAndSendPackage(process->fileDescriptor,COD_KC_PRINT_STDOUT,dataRequest->sizeBuffer,dataRequest->buffer);
-	if(package!=NULL){
-		logError("No se pudo comunicar a la consola el print del proceso %d",process->pid);
+	package = createAndSendPackage(process->fileDescriptor, COD_KC_PRINT_STDOUT,
+			dataRequest->sizeBuffer, dataRequest->buffer);
+	if (package != NULL) {
+		logInfo(
+				"Se indico a la consola %d imprimir el mensaje %s para el proceso %d",
+				process->fileDescriptor, dataRequest->buffer, process->pid);
+	} else {
+		logError("No se pudo comunicar a la consola el print del proceso %d",
+				process->pid);
 	}
 	destroyPackage(package);
 }
