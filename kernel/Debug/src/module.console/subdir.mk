dcc2421a35dfc1c7ac13cd95921fced560139d61
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-07-08 16:18:26 -0300

    makefile refactor

--- kernel/Debug/src/module.console/subdir.mk
+++ kernel/Debug/src/module.console/subdir.mk
@@ -23,7 +23,7 @@ C_DEPS += \
 src/module.console/%.o: ../src/module.console/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/Desarrollo/workspace/esther2017/dc-commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/esther2017/dc-commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
