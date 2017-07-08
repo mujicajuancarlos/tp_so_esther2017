5dd85c31a5c516d92ce77379c0fd87dada1690dc
mujicajuancarlos <mujica.juancarlos@gmail.com>
2017-07-08 16:33:35 -0300

    add dc commons make

---
+++ dc-commons/Debug/src/build/dc-commons/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../src/build/dc-commons/config.o \
+../src/build/dc-commons/logger.o \
+../src/build/dc-commons/package.o \
+../src/build/dc-commons/protocol-fileSystem-kernel.o \
+../src/build/dc-commons/protocol-kernel-cpu.o \
+../src/build/dc-commons/protocol-memory-core.o \
+../src/build/dc-commons/protocol-memory-kernel.o \
+../src/build/dc-commons/serialization.o \
+../src/build/dc-commons/socket-client.o \
+../src/build/dc-commons/socket-message.o \
+../src/build/dc-commons/socket-server.o \
+../src/build/dc-commons/stdout-printer.o \
+../src/build/dc-commons/utils.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
